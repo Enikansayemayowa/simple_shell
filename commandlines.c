a1a2eb2ea04af1172f9553228e40055259e72466
Enikansayemayowa <Enikansayemayowa405@gmail.com>
2023-05-23 16:24:37 -0700

    bebugging path

--- commandlines.c
+++ commandlines.c
@@ -4,10 +4,68 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 
 #define COMMAND_LENGTH 100
 #define MAX_ARGUMENTS 10
 #define PATH_LENGTH 100
+
+/* handle command lines with argument */
+/**
+ * command_line - Handle command lines with arguments
+ * @command: command to be passed
+ * @path: path to the argument
+ * Return: char
+ */
+char *command_line(char *command, char *path)
+{
+	char direction[PATH_LENGTH];
+	char *tok;
+
+	tok = strtok(path, ":");
+	while (tok != NULL)
+	{
+		snprintf(direction, PATH_LENGTH, "%s/%s", tok, command);
+		if (access(direction, X_OK) == 0)
+			return (strdup(direction));
+		tok = strtok(NULL, ":");
+	}
+	return (NULL);
+}
+/**
+ * file_path - Get the file path if it exists
+ * @file: file to check the path for
+ * Return: char
+ */
+char *file_path(char *file)
+{
+    struct stat st;
+    if (stat(file, &st) == 0 && S_ISREG(st.st_mode))
+        return realpath(file, NULL);
+    return NULL;
+}
+
+/* handle command path */
+/**
+ * command_path - Handle the PATH
+ * @command: command to be passed
+ * @arguments: the argument to be passed
+ * Return: void
+ */
+void command_path(char *command, char **arguments)
+{
+	int j = 0;
+	char *tok;
+
+	tok = strtok(command, " ");
+	while (tok != NULL && j < MAX_ARGUMENTS)
+	{
+		arguments[j] = tok;
+		tok = strtok(NULL, " ");
+		j++;
+	}
+	arguments[j] = NULL;
+}
 /**
  * main - Write a UNIX command line interpreter
  * Return: 0
@@ -15,15 +73,14 @@
 int main(void)
 {
 	char command[COMMAND_LENGTH], *arguments[MAX_ARGUMENTS + 1];
+	char *execArgs[3], *executable, *path;
 	int status;
 	pid_t pid;
-	char *execArgs[3], *executable, *path;
+
 	path = getenv("PATH");
-
-
 	while (1)
 	{
-		write(STDOUT_FILENO, "cisfun# ", 7);
+		write(STDOUT_FILENO, "cisfun# ", 8);
 		if (!fgets(command, sizeof(command), stdin))
 			break;
 		command[strcspn(command, "\n")] = '\0';
@@ -31,9 +88,9 @@ int main(void)
 			break;
 		command_path(command, arguments);
 		executable = command_line(arguments[0], path);
-		if (exucutable == NULL)
+		if (executable == NULL)
 		{
-			write(STDOUT_FILENO, "Command not found\n", 18);
+			write(STDOUT_FILENO, "No such file or directory\n", 26);
 			continue;
 		}
 		pid = fork();
@@ -44,7 +101,7 @@ int main(void)
 		}
 		else if (pid == 0)
 		{
-			execArgs[0] = command;
+			execArgs[0] = arguments[0];
 			execArgs[1] = NULL;
 			execve(executable, execArgs, NULL);
 			perror("execve");
@@ -55,44 +112,4 @@ int main(void)
 		free(executable);
 	}
 	return (0);
-}
-/* handle command lines with argument */
-/**
- * command_line - Handle command lines with arguments
- * @command: command to be passed
- * @path: path to the argument
- * Return: char
- */
-char command_line(char *command, char *path)
-{
-	char *direction;
-	char *tok;
-	tok = strtok(path, ":");
-	while (tok != NULL)
-	{
-		snprintf(dir, PATH_LENGTH, "%s/%s", tok, command);
-		if (access(direction, X_OK) == 0)
-			return (strdup(direction));
-		tok = strtok(NULL, ":");
-	}
-	return (NULL);
-}
-/* handle command path */
-/**
- * command_line - Handle the PATH
- * @command: command to be passed
- * @path: path to the argument
- * Return: void
- */
-void command_path(char *command, char **arguments)
-{
-	int j = 0;
-	char *tok = strtok(command, " ");
-	while (tok != NULL && < MAX_ARGUMENTS)
-	{
-		arguments[j] = tok;
-		tok = strtok(NULL, " ");
-		j++;
-	}
-	arguments[j] = NULL;
 }
--- main.h
+++ main.h
@@ -1,4 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 int main(void);
+void command_path(char *command, char **arguments);
+char command_line(char *command, char *path);
 #endif
